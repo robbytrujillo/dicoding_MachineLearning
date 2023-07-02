477b9e7b695a8568903d868214bc8017ed5edfa1
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-07-02 09:56:40 +0700

    machinelearning for python

---
+++ normalization.py
@@ -0,0 +1,10 @@
+from sklearn.preprocessing import MinMaxScaler
+
+data = [[12000000, 33], [35000000, 45], [4000000],23, [6500000, 26], [9000000, 29]]
+
+# object MinMaxScaler
+#---------------------
+scaler = MinMaxScaler()
+scaler.fit(data)
+
+print(scaler.transform(data))
