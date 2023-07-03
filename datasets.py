f5f475f48ab3af2740eb64b4afb5149d153b11e5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-07-03 16:49:41 +0700

    machinelearning for python | datasets

--- datasets.py
+++ datasets.py
@@ -1,3 +1,20 @@
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+# kode untuk memahami penentuan random_state_bekerja pada dataset
+from sklearn.model_selection import train_test_split
+
+X_data = range(10)
+y_data = range(10)
+
+print("random_state ditentukan")
+for i in range(3):
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.3, random_state = 42)
+    print(y_test)
+    
+    
+print("\n\nranodm_state tidak ditentukan")
+for i in range(3):
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.3, random_state = None)
+    print(y_test)
